fc129fc3f1bea8d2274dba14d574ed08f81c0c12
chs <chenhongshuo@koolearn-inc.com>
2024-06-24 23:57:14 +0800

    dist

--- .umirc.ts
+++ .umirc.ts
@@ -10,80 +10,80 @@ export default defineConfig({
     title: 'Sure-Tool-Demo',
   },
   routes: [
-    {
-      path: '/',
-      redirect: '/home',
-    },
-    {
-      name: '首页',
-      path: '/home',
-      component: './Home',
-    },
-    {
-      name: '权限演示',
-      path: '/access',
-      component: './Access',
-    },
-    {
-      name: ' CRUD 示例',
-      path: '/table',
-      component: './Table',
-    },
-    {
-      name: 'sql编辑器',
-      path: '/sql-editor',
-      component: './SqlEditor',
-    },
-    {
-      name: 'Context测试',
-      path: '/context',
-      component: './Context',
-    },
-    {
-      name: 'WebComponents',
-      path: '/WebComponents',
-      component: './WebComponents',
-    },
-    {
-      name: 'React Hooks',
-      path: '/react-hooks-demo',
-      component: './ReactHooks',
-    },
-    {
-      name: 'Markdown',
-      path: '/md-demo',
-      component: './Markdown',
-    },
-    {
-      name: 'Markdown-Marked',
-      path: '/md-marked',
-      component: './MarkdownMarked',
-    },
-    {
-      name: 'EditorTable-Antd',
-      path: '/editor-table-antd',
-      component: './EditorTableAntd',
-    },
-    {
-      name: 'ReactGridLayout',
-      path: '/ReactGridLayout',
-      component: './ReactGridLayout',
-    },
-    {
-      name: 'ReactConditionFilterDemo',
-      path: '/ReactConditionFilterDemo',
-      component: './ReactConditionFilterDemo',
-    },
-    {
-      name: 'ReduxDemo',
-      path: '/ReduxDemo',
-      component: './ReduxDemo',
-    },
-    {
-      name: 'Generator',
-      path: '/Generator',
-      component: './Generator',
-    },
+    // {
+    //   path: '/',
+    //   redirect: '/home',
+    // },
+    // {
+    //   name: '首页',
+    //   path: '/home',
+    //   component: './Home',
+    // },
+    // {
+    //   name: '权限演示',
+    //   path: '/access',
+    //   component: './Access',
+    // },
+    // {
+    //   name: ' CRUD 示例',
+    //   path: '/table',
+    //   component: './Table',
+    // },
+    // {
+    //   name: 'sql编辑器',
+    //   path: '/sql-editor',
+    //   component: './SqlEditor',
+    // },
+    // {
+    //   name: 'Context测试',
+    //   path: '/context',
+    //   component: './Context',
+    // },
+    // {
+    //   name: 'WebComponents',
+    //   path: '/WebComponents',
+    //   component: './WebComponents',
+    // },
+    // {
+    //   name: 'React Hooks',
+    //   path: '/react-hooks-demo',
+    //   component: './ReactHooks',
+    // },
+    // {
+    //   name: 'Markdown',
+    //   path: '/md-demo',
+    //   component: './Markdown',
+    // },
+    // {
+    //   name: 'Markdown-Marked',
+    //   path: '/md-marked',
+    //   component: './MarkdownMarked',
+    // },
+    // {
+    //   name: 'EditorTable-Antd',
+    //   path: '/editor-table-antd',
+    //   component: './EditorTableAntd',
+    // },
+    // {
+    //   name: 'ReactGridLayout',
+    //   path: '/ReactGridLayout',
+    //   component: './ReactGridLayout',
+    // },
+    // {
+    //   name: 'ReactConditionFilterDemo',
+    //   path: '/ReactConditionFilterDemo',
+    //   component: './ReactConditionFilterDemo',
+    // },
+    // {
+    //   name: 'ReduxDemo',
+    //   path: '/ReduxDemo',
+    //   component: './ReduxDemo',
+    // },
+    // {
+    //   name: 'Generator',
+    //   path: '/Generator',
+    //   component: './Generator',
+    // },
     {
       name: 'UNO',
       path: '/UNO',
@@ -91,5 +91,6 @@ export default defineConfig({
     }
   ],
   npmClient: 'pnpm',
+  esbuildMinifyIIFE: true,
 });
 
